9210ec55f3100ba6da3e28fa6dbe697fb46061f7
RashidMall <mr.mallaev@gmail.com>
2018-09-17 11:57:55 +0200

    Add create edit a giveaway

--- src/Controller/GiveawayController.php
+++ src/Controller/GiveawayController.php
@@ -2,8 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Giveaway;
+use App\Form\GiveawayType;
+use App\Repository\GiveawayRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Doctrine\Common\Persistence\ObjectManager;
 
 class GiveawayController extends AbstractController
 {
@@ -14,6 +22,36 @@ class GiveawayController extends AbstractController
     {
         return $this->render('giveaway/giveaway.html.twig', [
             'controller_name' => 'GiveawayController',
+        ]);
+    }
+
+    /**
+     * @Route("/giveaway/new", name="new_giveaway")
+     * @Route("/giveaway/{id}/edit", name="edit_giveaway")
+     */
+    public function form(Request $request, ObjectManager $manager, Giveaway $giveaway = null){
+        if(!$giveaway){
+            $giveaway = new Giveaway();
+        }
+
+        $form = $this->createForm(GiveawayType::class, $giveaway);
+
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted() && $form->isValid()){
+            if(!$giveaway->getId()){
+                $giveaway->setCreatedAt(new \DateTime());
+            }
+
+            $manager->persist($giveaway);
+            $manager->flush();
+
+            return $this->redirectToRoute('show_giveaway', ['id' => $giveaway->getId()]);
+        }
+        
+        return $this->render('giveaway/new.html.twig', [
+            'formGiveaway' => $form->createView(),
+            'editMode' => $giveaway->getId() !== null
         ]);
     }
 }
